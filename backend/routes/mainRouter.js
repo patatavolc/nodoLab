8871a131ed064f03417fab10519342c35f6a7b89
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-11-12 19:38:15 +0100

    Feat pagos.routes

--- backend/routes/mainRouter.js
+++ backend/routes/mainRouter.js
@@ -4,9 +4,13 @@ const router = express.Router();
 import usuariosRoutes from './usuarios.routes.js';
 import datosBancariosRoutes from './datosBancarios.routes.js';
 import recursosRoutes from './recursos.routes.js';
+import reservasRoutes from './reservas.routes.js';
+import pagosRoutes from './pagos.routes.js';
 
 router.use(usuariosRoutes);
 router.use(datosBancariosRoutes);
 router.use(recursosRoutes);
+router.use(reservasRoutes);
+router.use(pagosRoutes);
 
 export default router; 
