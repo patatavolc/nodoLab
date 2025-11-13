430a0d9fb93b4dd66ec75c9697fabe32591f7cec
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-11-13 14:05:35 +0100

    Feat: crear dato bancario

---
+++ backend/services/datosBancarios.service.js
@@ -0,0 +1,12 @@
+import pool from '../../database/db.js';
+
+//Crear nuevo dato bancario
+export const newDatoBancario = async (data) => {
+    const {id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV} = data;
+
+    const result = await pool.query(
+        'INSERT INTO datos_bancarios(id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV]
+    );
+    return result.rows[0];
+}
