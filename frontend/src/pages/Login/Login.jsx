15b14e60e4a71abb68484a353181b348d64b0185
CarlosDZ <carlos.dz16180@gmail.com>
2025-11-18 20:22:52 +0100

    Feat: El login crea la cookie de sesion

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
         console.log("submitted");
 
         try {
-            const response = await fetch("http://localhost:3000/api/login", {
+            const response = await fetch("http://localhost:3000/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
