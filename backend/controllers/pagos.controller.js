877619552f6b9a06db9d21231a42998502945b24
PabloGargalloSanz <pablogargallosanz@gmail.com>
2025-11-25 17:32:37 +0100

    Feat: pagos.controller

--- backend/controllers/pagos.controller.js
+++ backend/controllers/pagos.controller.js
@@ -16,3 +16,77 @@ export const createPago = (req, res) => {
         res.status(400).send({error: 'Faltan datos obligatorios'});
     }
 }
+
+//Obtener todos pagos
+export const getPagos = (req,res) => {
+
+    getAllPagos()
+        .then((pagos) =>{
+            res.send(pagos);
+        });
+}
+
+//Obtener pago por id
+export const getPagoById = (req, res) => {
+    const idPago = req.params.idPago;
+
+    if(idPago){
+
+        getPagoByIdService()
+            .then((pago) =>{
+                res.status(200).send(pago);
+            })
+            .catch((error) => {
+                res.status(400).send({error: error.message});
+            })
+        } else {
+            res.status(400).send({error:'Faltan datos obligatorios'});
+    }
+}
+
+//Obtener pago by id reserva
+export const getPagoByIdReserva = (req, res) => {
+    const idReserva= req.params.idReserva;
+
+    if(idReserva){
+        getPagoByIdReservaService()
+            .then((pago) =>{
+                res.status(200).send(pago);
+            })
+            .catch((error) => {
+                res.status(400).send({error: error.message});
+            })
+        } else {
+            res.status(400).send({error:'Faltan datos obligatorios'});
+    }
+}
+//Obtener pago by metodo
+export const getPagosByMetodoPago = (req, res) => {
+    const metodoPago= req.params.metodoPago;
+
+    if(metodoPago){
+        getPagoByMetodoService()
+            .then((pago) =>{
+                res.status(200).send(pago);
+            })
+            .catch((error) => {
+                res.status(400).send({error: error.message});
+            })
+        } else {
+            res.status(400).send({error:'Faltan datos obligatorios'});
+    }
+}
+
+//Actualizar pago
+export const updatePago = (req, res) => {
+    const data = req.body;
+    const idPago = req.params.idPago;
+
+    updatePagoService(idPago, data)
+        .then((updatedPago) =>{
+            res.status(200).send(updatedPago);
+        })
+        .catch((error) => {
+            res.status(400).send({error: error.message});
+        });
+}
