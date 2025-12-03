aa440568bbcb6d5ff3a42700c2257f037a7f0b79
EderMoros3 <edermorosrodero@gmail.com>
2025-12-03 16:54:27 +0100

    feat: getDashboardFinanzas function and refactor other controllers

--- backend/controllers/dashboard-controller.js
+++ backend/controllers/dashboard-controller.js
@@ -4,7 +4,6 @@ export const getDashboardData = async (req, res) => {
         const data = await getDashboardStats();
         res.status(200).json(data);
     } catch (error) {
-        console.error('Error en getDashboardData: ', error);
         res.status(500).json({error: error.message });
     }
 };
@@ -14,9 +13,17 @@ export const getDashboardResumen = async (req, res) => {
         const data = await getDashboardResumen();
         res.status(200).json(data);
     } catch(error) {
-        console.error('Error en getDashboardResumen: ', error);
         res.status(500).json({error: error.message });
     }
 };
+
+export const getDashboardFinanzas = async (req, res) => {
+    try {
+        const data = await getDashboardFinanzas();
+        res.status(200).json(data);
+    } catch(error) {
+        res.status(500).json({error: error.message });
+    }
+}
 
 
