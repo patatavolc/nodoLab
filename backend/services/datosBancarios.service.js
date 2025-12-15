aab55eec35a36bf977970c2c0d75f8e0a45a762f
jfmartinez10 <jfmartinez383@gmail.com>
2025-12-15 13:18:29 +0100

    feat: Obtener todos los datos bancarios desde el archivo datosBancarios.service.js

--- backend/services/datosBancarios.service.js
+++ backend/services/datosBancarios.service.js
@@ -15,3 +15,14 @@ export const newDatoBancario = async (data) => {
         throw new Error(`Error al crear un nuevo dato bancario: ${error.message}`);
     }
 };
+
+// Obtener todos los datos bancarios
+export const getDatosBancariosService = async () => {
+    try {
+        const result = await pool.query("SELECT * FROM datos_bancarios ORDER BY id_dato_bancario DESC");
+        return result.rows;
+    } catch (error) {
+        console.error("Error en el servicio getDatosBancariosService", error.message);
+        throw new Error(`Error al obtener todos los datos bancarios: ${error.message}`);
+    }
+};
