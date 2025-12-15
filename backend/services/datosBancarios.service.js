b50e1f02df0963687ca11877d7e221822abeb220
jfmartinez10 <jfmartinez383@gmail.com>
2025-12-15 13:14:38 +0100

    feat: Crear nuevo dato bancario desde el archivo datosBancarios.service.js

--- backend/services/datosBancarios.service.js
+++ backend/services/datosBancarios.service.js
@@ -2,11 +2,16 @@ import pool from '../database/db.js';
 
 //Crear nuevo dato bancario
 export const newDatoBancario = async (data) => {
-    const {id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV} = data;
+    try {
+        const {id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV} = data;
 
-    const result = await pool.query(
-        'INSERT INTO datos_bancarios(id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV) VALUES ($1, $2, $3, $4, $5) RETURNING *',
-        [id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV]
-    );
-    return result.rows[0];
-}
+        const result = await pool.query(
+            'INSERT INTO datos_bancarios(id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+            [id_usuario, IBAN, num_tarjeta, fecha_tarjeta, CVV]
+        );
+        return result.rows[0];
+    } catch (error) {
+        console.error("Error en el servicio newDatoBancario", error.message);
+        throw new Error(`Error al crear un nuevo dato bancario: ${error.message}`);
+    }
+};
