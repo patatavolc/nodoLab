b77d9700c277285586ec707741050f409caa5b45
EderMoros3 <edermorosrodero@gmail.com>
2025-12-17 17:04:50 +0100

    feat: add controller to recursos routes

--- backend/routes/recursos.routes.js
+++ backend/routes/recursos.routes.js
@@ -1,12 +1,20 @@
-import express from 'express'; 
+import express from "express";
 const router = express.Router();
 
-import { getRecursos, getRecursoById, createRecurso, updateRecurso, getRecursosByTipo} from '../controllers/recursos.controller.js'; 
+import {
+    getRecursos,
+    getRecursoById,
+    createRecurso,
+    updateRecurso,
+    getRecursosByTipo,
+    getRecursosDisponibles,
+} from "../controllers/recursos.controller.js";
 
-router.get('/recursos', getRecursos);
-router.get('/recursos/:id', getRecursoById);
-router.get('/recursos/tipo/:tipo', getRecursosByTipo);
-router.post('/recursos', createRecurso);
-router.put('/recursos/:id', updateRecurso);
+router.get("/recursos", getRecursos);
+router.get("/recursos/:id", getRecursoById);
+router.get("/recursos/tipo/:tipo", getRecursosByTipo);
+router.get("/recursos/disponibles", getRecursosDisponibles);
+router.post("/recursos", createRecurso);
+router.put("/recursos/:id", updateRecurso);
 
 export default router;
