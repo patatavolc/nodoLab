94c595a89e3d06a687a667f83e2ea810b08e37a7
CarlosDZ <carlos.dz16180@gmail.com>
2025-12-18 11:33:09 +0100

    minor fix

--- backend/routes/datosBancarios.routes.js
+++ backend/routes/datosBancarios.routes.js
@@ -1,12 +1,17 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
 
-import { getDatosBancarios, createDatoBancario, updateDatosBancarios, getDatosBancariosByDni } from '../controllers/datosBancarios.controller.js'; 
-import { authMiddleware, isAdmin } from '../middleware/auth.middeware.js';
+import {
+    getDatosBancarios,
+    createDatoBancario,
+    updateDatosBancarios,
+    getDatosBancariosByDni,
+} from "../controllers/datosBancarios.controller.js";
+import { authMiddleware, isAdmin } from "../middleware/auth.middeware.js";
 
-router.get('/datosBancarios', authMiddleware, isAdmin, getDatosBancarios);
-router.get('/datosBancarios/:dni', authMiddleware, isAdmin, getDatosBancariosByDni);
-router.post('/datosBancarios', authMiddleware, isAdmin, createDatoBancario);
-router.put('/datosBancarios/:id', authMiddleware, isAdmin, updateDatosBancarios);
+router.get("/datosBancarios", getDatosBancarios);
+router.get("/datosBancarios/:dni", getDatosBancariosByDni);
+router.post("/datosBancarios", createDatoBancario);
+router.put("/datosBancarios/:id", authMiddleware, isAdmin, updateDatosBancarios);
 
 export default router;
