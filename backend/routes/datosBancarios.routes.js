3da06ce6e468c0aa4db7681080a382076f0b87c3
CarlosDZ <carlos.dz16180@gmail.com>
2025-12-18 11:17:56 +0100

    Feat: Rutas de datos bancarios limitadas a admins

--- backend/routes/datosBancarios.routes.js
+++ backend/routes/datosBancarios.routes.js
@@ -2,10 +2,11 @@ import express from 'express';
 const router = express.Router();
 
 import { getDatosBancarios, createDatoBancario, updateDatosBancarios, getDatosBancariosByDni } from '../controllers/datosBancarios.controller.js'; 
+import { authMiddleware, isAdmin } from '../middleware/auth.middeware.js';
 
-router.get('/datosBancarios', getDatosBancarios);
-router.get('/datosBancarios/:dni', getDatosBancariosByDni);
-router.post('/datosBancarios', createDatoBancario);
-router.put('/datosBancarios/:id', updateDatosBancarios);
+router.get('/datosBancarios', authMiddleware, isAdmin, getDatosBancarios);
+router.get('/datosBancarios/:dni', authMiddleware, isAdmin, getDatosBancariosByDni);
+router.post('/datosBancarios', authMiddleware, isAdmin, createDatoBancario);
+router.put('/datosBancarios/:id', authMiddleware, isAdmin, updateDatosBancarios);
 
 export default router;
