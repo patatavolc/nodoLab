a34f55437a81c691f8743728c94ef05475af1440
CarlosDZ <carlos.dz16180@gmail.com>
2026-01-07 11:39:09 +0100

    funcion de login en frontend

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -1,1 +1,20 @@
 // Logica de login, registro y guardar token
+export async function login(nameOrMail, password) {
+    const res = await fetch("/api/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            email: nameOrMail,
+            password: password,
+        }),
+    });
+    if (!res.ok) {
+        const errorBody = await res.text().catch(() => "");
+        throw new Error(`Error ${res.status}: ${errorBody}`);
+    }
+
+    const data = await res.json();
+    localStorage.setItem("nodoLabAuthToken", data.token);
+
+    return data ?? new Error("Error desconocido xd");
+}
