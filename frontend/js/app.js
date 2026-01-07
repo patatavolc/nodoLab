69070d064707ec2cc8eb27e5ee4bd60d5fc6665d
CarlosDZ <carlos.dz16180@gmail.com>
2026-01-07 11:56:48 +0100

    funcion para crear los recursos en frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,1 +1,22 @@
 // Logica para las cosas de pantalla
+import { apiFetch } from "./api";
+
+export async function getRecursos() {
+    const res = await fetch("/api/recursos");
+    if (!res.ok) {
+        const errorBody = await res.text().catch(() => "");
+        throw new Error(`Error ${res.status}: ${errorBody}`);
+    }
+
+    const data = await res.json();
+
+    const contenedor = document.querySelector("#contenedor-de-recursos");   //cambiar el id
+    contenedor.innerHTML = "";
+
+    data.forEach((recurso) => {
+        const card = document.createElement("div");
+        card.classList.add("nombre-de-la-clase-de-los-recursos");   //cambiar el nombre de la clase
+        card.innerHTML = `Aqui metemos el html. Si queremos pillar un dato de recurso podemos hacer <h1>${recurso.nombre}</h1> por ejemplo. Esto ya lo dejo a gusto de los que diseñen`;
+        contenedor.appendChild(card);
+    });
+}
